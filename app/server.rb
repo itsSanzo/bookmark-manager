a5a5f4c83e1ba8026f63a1f5c91babc30cba49a0
itsmurasaki <itsmurasaki@gmail.com>
2014-03-24 18:00:46 +0000

    password confirmation

--- app/server.rb
+++ app/server.rb
@@ -35,7 +35,9 @@ get "/users/new" do
 end
 
 post "/users" do
-  user = User.create(:email => params[:email], :password => params[:password])
+  user = User.create(:email => params[:email], 
+                     :password => params[:password],
+                     :password_confirmation => params[:password_confirmation])
   session[:user_id] = user.id 
   redirect to('/')
 end
