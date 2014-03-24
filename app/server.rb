dae893aecbba6c0a59b879779b3e19bc248cbc48
itsmurasaki <itsmurasaki@gmail.com>
2014-03-24 18:21:16 +0000

    handling pass matching error

--- app/server.rb
+++ app/server.rb
@@ -1,13 +1,16 @@
 require 'sinatra'
 require 'data_mapper'
+require 'rack-flash'
 require './lib/link'
 require './lib/tag'
 require './lib/user'
+
 require_relative 'helpers/application'
 require_relative 'data_mapper_setup'
 
 enable :sessions
 set :session_secret, 'super secret key!!!'
+use Rack::Flash
 
 get "/" do 
   @links = Link.all
@@ -31,13 +34,19 @@ get "/tags/:text" do
 end
 
 get "/users/new" do
+  @user = User.new
   erb :"users/new"
 end
 
 post "/users" do
-  user = User.create(:email => params[:email], 
+  @user = User.new(:email => params[:email], 
                      :password => params[:password],
                      :password_confirmation => params[:password_confirmation])
-  session[:user_id] = user.id 
-  redirect to('/')
+  if @user.save
+    session[:user_id] = @user.id 
+    redirect to('/')
+  else
+    flash[:notice] = "Sorry, your passwords don't match"
+    erb :"users/new"
+  end
 end
