1e379230f200267ed2f484fa4e6864f5aa7aae2b
itsmurasaki <itsmurasaki@gmail.com>
2014-06-02 19:09:25 +0100

    password confirmation needed

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -7,11 +7,17 @@ feature "User sign up" do
     expect(User.first.email).to eq("ciao@ciao.com")
   end
 
+  scenario "with a password that doesn't match" do
+    lambda { sign_up('no@no.com', 'psw', 'wrong') }.should change(User, :count).by(0)
+  end
+
   def sign_up(email = "ciao@ciao.com",
-              password = "12345678")
+              password = "12345678",
+              password_confirmation = "12345678")
     visit '/users/new'
     fill_in :email, :with => email
     fill_in :password, :with => password
+    fill_in :password_confirmation, :with => password_confirmation
     click_button "Sign up"
   end
 end
