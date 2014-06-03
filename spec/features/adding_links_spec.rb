abec157ee863a7c73610dd14d1b8539de73aa030
itsmurasaki <itsmurasaki@gmail.com>
2014-06-03 02:22:46 +0100

    added 'add new link' button

--- spec/features/adding_links_spec.rb
+++ spec/features/adding_links_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature "User adds a new link" do
   scenario "when browsing the homepage" do
     expect(Link.count).to eq(0)
-    visit "/"
+    visit "/links/new"
     add_link("http://www.makersacademy.com/", "Makers Academy")
     expect(Link.count).to eq(1)
     link = Link.first
@@ -12,7 +12,7 @@ feature "User adds a new link" do
   end
 
   scenario "with a few tags" do
-    visit "/"
+    visit "/links/new"
     add_link("http://www.makersacademy.com/", "Makers Academy", ["education", "ruby"])
     link = Link.first
     expect(link.tags.map(&:text)).to include("education")
@@ -20,7 +20,7 @@ feature "User adds a new link" do
   end
 
   def add_link(url, title, tags = [])
-    within('#new-link') do
+    within('.new-link') do
       fill_in 'url', :with => url
       fill_in 'title', :with => title
       fill_in 'tags', :with => tags.join(' ')
